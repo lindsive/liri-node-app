41982907f313c403bddbd92da6146a85c5206b1b
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-07-30 20:14:32 -0400

    initial push

---
+++ liri.js
@@ -0,0 +1,13 @@
+// require npms
+var axios = require("axios");
+var spotifty = require('node-spotify-api');
+var dotenv = require('dotenv').config()
+
+
+// omdb variables
+var omdbSearch = 
+var omdbURL = "http://www.omdbapi.com/?t=Frozen&apikey=trilogy"
+
+
+
+
