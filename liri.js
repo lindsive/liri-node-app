11419e9d6ee0e267719a5478e2c208f8d26885a2
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-07-31 22:26:34 -0400

    added switch case, 2 command functions

--- liri.js
+++ liri.js
@@ -1,49 +1,99 @@
 // require npms
 var axios = require("axios");
-// var spotifty = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 
-// constructor
-var spotifty = new spotifty(keys.spotify);
+
+var spotify = new Spotify(keys.spotify);
 
 
 var dotenv = require('dotenv').config();
 var keys = require("./keys.js");
 
+var command = process.argv[2];
+var search = process.argv[3];
+
+// bands in town api
+var bandsAPI = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
+
 
 // commands
 // use constructors for each type of command
 
-    // "concert-this"
-        // searches bands in town artist events api
-        // api: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-            // name of venue; venue location; date of event mm/dd/yyyy
-    
-    
+// "concert-this"
+// searches bands in town artist events api
+// api: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+// name of venue; venue location; date of event mm/dd/yyyy
+// make function in constructor w api key
 
-    // "spotify-this-song"
-        // show info abt show in terminal:
-            // artist; song name; preview link of song from spotify; album song is from
-            // if no song provided, default to "the sign" by Ace of Base
-            // will use:
-                // node-spotify-api package
-                // client and secret IDs needed
 
-    // "movie-this"
-        // output in terminal:
-            // Title of the movie.
-            // Year the movie came out.
-            // IMDB Rating of the movie.
-            // Rotten Tomatoes Rating of the movie.
-            // Country where the movie was produced.
-            // Language of the movie.
-            // Plot of the movie.
-            // Actors in the movie.
 
-        // default: "mr. nobody" 
-        // with <http://www.imdb.com/title/tt0485947/>
-        // it's on netflix!
-        // usee axios package to retrieve from ombd api
 
-    // "do-what-it-says"
-        // fs.readFile random.txt
+
+function concert() {
+
+    axios.get(bandsAPI).then(
+        function(response) {
+            for (var i = 0; i < response.data.length; i++) {
+            console.log(response.data[i])
+            console.log("test")
+            }
+        }).catch(function (error) {
+
+            console.log(error)
+        })
+   
+}
+
+
+
+
+// "spotify-this-song"
+// show info abt show in terminal:
+// artist; song name; preview link of song from spotify; album song is from
+// if no song provided, default to "the sign" by Ace of Base
+// will use:
+// node-spotify-api package
+// client and secret IDs needed
+
+function song(){
+    spotify.search({ type: 'track', query: search }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       var results = data.tracks.items
+      for (i = 0; i < results.length; i++) {
+          console.log(results[i].name)
+      }
+      });
+}
+
+
+// "movie-this"
+// output in terminal:
+// Title of the movie.
+// Year the movie came out.
+// IMDB Rating of the movie.
+// Rotten Tomatoes Rating of the movie.
+// Country where the movie was produced.
+// Language of the movie.
+// Plot of the movie.
+// Actors in the movie.
+
+// default: "mr. nobody" 
+// with <http://www.imdb.com/title/tt0485947/>
+// it's on netflix!
+// usee axios package to retrieve from ombd api
+
+// "do-what-it-says"
+// fs.readFile random.txt
+
+switch (command) {
+    case "concert-this":
+        concert();
+        break;
+    case "spotify-this-song":
+        song();
+        break;
+
+}
 
