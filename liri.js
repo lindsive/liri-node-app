484b3f41f5fbaa99e41675c8c6cc47172114d5ed
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-08-02 16:26:07 -0400

    added questions; added song() for command spotify-this-song

--- liri.js
+++ liri.js
@@ -1,49 +1,45 @@
-// require npms
+// questions:
+// moment.js for concert();
+// default for song() "spotify-this-song command"
+
+
+// require npms/files
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
-
+var moment = require('moment');
+moment().format();
 var dotenv = require('dotenv').config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-
-
-
+// command like vars
 var command = process.argv[2];
 var search = process.argv.slice(3).join(" ");
+var musicArtist = process.argv.slice(4).join(" ");
 
+// COMMAND "concert-this"
 // bands in town api
 var bandsAPI = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
 
 
-// commands
-// use constructors for each type of command
-
-// "concert-this"
-// searches bands in town artist events api
-// api: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-// name of venue; venue location; date of event mm/dd/yyyy
-// make function in constructor w api key
-
-
-
-
-
+// COMMAND "concert-this function"
 function concert() {
-console.log(bandsAPI)
     axios.get(bandsAPI).then(
-        function(response) {
-            console.log(response.data)
+        function (response) {
+
             for (var i = 0; i < response.data.length; i++) {
-            console.log(response.data[i])
-            console.log("test")
+                console.log("Artist: " + response.data[i].lineup[i]);
+                console.log("Venue: " + response.data[i].venue.name + ", in " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+
+                var time = response.data[i].datetime
+                console.log("Date: " + time);
+                moment(time, "MM-DD-YYYY");
+
             }
         }).catch(function (error) {
-
             console.log(error)
         })
-   
-}
+};
 
 
 
@@ -56,16 +52,24 @@ console.log(bandsAPI)
 // node-spotify-api package
 // client and secret IDs needed
 
-function song(){
-    spotify.search({ type: 'track', query: search }, function(err, data) {
+function song() {
+    spotify.search({ type: 'track', query: search }, function (err, data) {
+
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
+
+        } else {
+        var results = data.tracks.items
+        for (i = 0; i < results.length; i++) {
+            // console.log(results[i]);
+            console.log("========================");
+            console.log("Artist: " + results[i].artists[0].name);
+            console.log("Song name: " + results[i].name)
+            console.log("Album: " + results[i].album.name);
+            console.log("Link to Spotify: " + results[i].external_urls.spotify);
         }
-       var results = data.tracks.items
-      for (i = 0; i < results.length; i++) {
-          console.log(results[i].name)
-      }
-      });
+    }
+    });
 }
 
 
